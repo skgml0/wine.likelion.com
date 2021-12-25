eae4354a4d8bca60b6a2103f976419a0bdd73fb3
skgml0 <hnh2714@gmail.com>
2021-12-26 01:52:38 +0900

    💄 WineCard CSS 적용 및 파일 삭제

--- src/components/WineCard.tsx
+++ src/components/WineCard.tsx
@@ -1,14 +1,44 @@
+import styled from "@emotion/styled";
 import { Wine } from "../types/Wine";
+
 interface WineProps {
-    wineData: Wine
+  wineData: Wine;
 }
+
 export const WineCard = ({ wineData }: WineProps) => {
-  const { wine, winery } = wineData;
+  const { wine, winery, image, location, rating } = wineData;
 
   return (
-    <div>
-      <h1>{wine}</h1>
-      <p>{winery}</p>
-    </div>
-  )
-}
+    <Container>
+      <img src={image} alt="" />
+      <h2>
+        {wine}
+        <Average>{rating.average}</Average>
+      </h2>
+      <p>
+        {winery} - {location}
+      </p>
+      <p>후기 : {rating.reviews.replace(" ratings", "")}</p>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 1em;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+  border-radius: 1em;
+`;
+
+const Average = styled.div`
+  display: inline-block;
+  padding: 0.3em;
+  font-size: 16px;
+  margin-left: 0.5em;
+  color: white;
+  background: #2ac1bc;
+  border-radius: 3px;
+`;
---
+++ src/components/WineContainer.tsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import { Error, Loading, WineCard } from ".";
+import { MEDIA_QUERY_END_POINT } from "../constants";
+import { useWineData } from "../hooks/useWineData";
+import { Wine } from "../types/Wine";
+
+interface WineContainerProps {
+  name: string
+}
+
+export const WineContainer = ({ name }: WineContainerProps) => {
+  const { data, error } = useWineData(name)
+  
+  if(error) return <Error />
+  if(!data) return <Loading />
+  
+  return (
+    <div>
+      <WineTitle>{name} wine</WineTitle>
+      <WineCardContainer>
+        {data.map((wineData: Wine) => {
+          return (
+            <WineCard 
+              key={`${name}-wine-list-${wineData.id}`} 
+              wineData={wineData}
+            />
+          )
+        })}
+      </WineCardContainer>
+    </div>
+  )
+}
+
+const WineTitle = styled.h1`
+  line-height: 180%;
+  border-bottom: 1px solid #eee;
+`
+
+const WineCardContainer = styled.main`
+  display: grid;
+  gap: 1em;
+  @media (min-width: ${MEDIA_QUERY_END_POINT.MOBILE}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (min-width: ${MEDIA_QUERY_END_POINT.TABLET}) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`
