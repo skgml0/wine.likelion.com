ef3e21ee884e49c822ee8f82f461afc214928935
skgml0 <hnh2714@gmail.com>
2021-12-26 01:58:29 +0900

    ✨ 맥주 API 연동

--- src/hooks/useBeerData.ts
+++ src/hooks/useBeerData.ts
@@ -4,4 +4,6 @@ import { BEER_API_ENDPOINT } from '../constants';
 
 export const useBeerData = (path: string) => {
   return useSWR(`${BEER_API_ENDPOINT}${path}`, fetcher);
+  // fetcher 데이터 받아오기 위해
+  // useSWR 데이터가 잘받아왔는지 상태관리 / SWR 상태관리 라이브러리 
 }
